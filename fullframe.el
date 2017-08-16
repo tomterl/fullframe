d6a5217f7f2a5a5edcb75140f3fa69b3a50f1cdd
Tom Regner <tom@goochesa.de>
2017-08-16 12:03:42 +0200

    Version bump (0.5.0)

--- fullframe.el
+++ fullframe.el
@@ -4,7 +4,7 @@
 
 ;; Author: Tom Regner <tom@goochesa.de>
 ;; Maintainer: Tom Regner <tom@goochesa.de>
-;; Version: 0.4.9
+;; Version: 0.5.0
 ;; Keywords: fullscreen
 ;; Package-Requires: ((cl-lib "0.5"))
 
